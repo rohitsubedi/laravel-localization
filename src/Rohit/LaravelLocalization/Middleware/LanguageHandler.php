41c151e87c5a2d55b084acc72ac77e0447da4c3f
Rohit Subedi <rohitsubedi@gmail.com>
2016-06-11 10:49:47 +0700

    Refactored Middleware

--- src/Rohit/LaravelLocalization/Middleware/LanguageHandler.php
+++ src/Rohit/LaravelLocalization/Middleware/LanguageHandler.php
@@ -19,22 +19,20 @@ class LanguageHandler
         $allLocales = config('laravel-localization.all_locales', []);
         $default    = config('laravel-localization.default_locale', '');
 
-        if (strlen($default)) {
-            if (in_array($request->segment(1), $allLocales))
-            {
-                if($request->segment(1) === $default) {
-                    array_walk($allLocales, function(&$item, $key) {
-                        $item = '/' . $item . '/';
-                    });
+        if (in_array($request->segment(1), $allLocales))
+        {
+            if($request->segment(1) === $default) {
+                array_walk($allLocales, function(&$item, $key) {
+                    $item = '/' . $item . '/';
+                });
 
-                    $url = str_replace($allLocales, '/', $request->server('REQUEST_URI'));
+                $url = str_replace($allLocales, '/', $request->server('REQUEST_URI'));
 
-                    if ($url === '/' . $default) {
-                        $url = '/';
-                    }
-
-                    return redirect($url);
+                if ($url === '/' . $default) {
+                    $url = '/';
                 }
+
+                return redirect($url);
             }
         }
 
